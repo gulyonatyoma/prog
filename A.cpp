8d5021d1c3b83a4e8ac7e7b2af6eed774d10ebbf
gulyonatyoma <artyomgostev26082005@gmail.com>
2023-12-04 14:57:41 +0300

    Update A

--- A.cpp
+++ A.cpp
@@ -1,10 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main(){
     int n; cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    sort(a.begin(), a.end());
-    for (auto& it : a) cout << it << ' ';
+    cout << n;
     return 0;
 }
