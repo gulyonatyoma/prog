f4e5c3cc827718228f7fc7aa07ff845431c7509c
gulyonatyoma <artyomgostev26082005@gmail.com>
2023-12-04 15:03:52 +0300

    Edit file

--- test_folder/rest.cpp
+++ test_folder/rest.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    cin >> n;
+    int n; cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++){
         cin >> a[i];
     }
-    for (int i = 0; i < n; i++) {
+    reverse(a.begin(), a.end());
+    for (int i = 0; i < n; i++){
         cout << a[i] << ' ';
     }
     return 0;
