e1df500ac6b35f683aa5c6f148c481d4d0e3d690
gulyonatyoma <artyomgostev26082005@gmail.com>
2023-12-04 15:00:09 +0300

    Sort Array

--- A.cpp
+++ A.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 int main(){
     int n; cin >> n;
-    cout << n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (auto& it : a) cout << it << ' ';
     return 0;
 }
